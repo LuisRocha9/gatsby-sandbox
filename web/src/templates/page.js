c2a6e38c39bc200b0c723e0e2951d7929a517bb5
Filipa Pereira <afilipaspereira@gmail.com>
2021-10-13 10:45:04 +0100

    add layout to pages and import global styles

--- web/src/templates/page.js
+++ web/src/templates/page.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 export default ({ pageContext }) => {
   return (
-    <div>
-      <h1>{pageContext.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
-    </div>
+    <Layout>
+      <div>
+        <h1>{pageContext.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
+      </div>
+    </Layout>
   );
 }
--- web/src/templates/post.js
+++ web/src/templates/post.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 export default ({ pageContext }) => {
   return (
-    <div>
-      <h2>{pageContext.title}</h2>
-      <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
-    </div>
+    <Layout>
+      <div>
+        <h2>{pageContext.title}</h2>
+        <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
+      </div>
+    </Layout>
   );
 }
