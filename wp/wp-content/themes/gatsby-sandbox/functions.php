7fd6d41c17d6cdb600eeb138e32d9e1215c3cc58
Filipa Pereira <afilipaspereira@gmail.com>
2021-10-13 12:09:06 +0100

    Register Main menu

--- wp/wp-content/themes/gatsby-sandbox/functions.php
+++ wp/wp-content/themes/gatsby-sandbox/functions.php
@@ -1,3 +1,9 @@
 <?php
 add_theme_support( 'post-thumbnails', array( 'post', 'page', 'project' ) );
+
+register_nav_menus(
+  array(
+    'menu-1' => esc_html__( 'Primary', 'gatsby-sandbox' ),
+  )
+);
 ?>
