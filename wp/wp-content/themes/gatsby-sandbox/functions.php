b5e96f887bf28f9943292d1905ac391c78557be7
Luis Rocha <luis.fcrocha9@gmail.com>
2021-10-14 17:40:24 +0100

    Add ACF Better Search plugin

--- wp/wp-content/themes/gatsby-sandbox/functions.php
+++ wp/wp-content/themes/gatsby-sandbox/functions.php
@@ -6,4 +6,15 @@ register_nav_menus(
     'menu-1' => esc_html__( 'Primary', 'gatsby-sandbox' ),
   )
 );
+
+
+add_action( 'graphql_register_types', function() {
+    register_graphql_field( 'Author', 'author', [
+        'type'        => 'String',
+        'description' => 'author meta value',
+        'resolve'     => function( \WPGraphQL\Model\Post $post, $args, $context, $info ) {
+            return 'Example string with the title of the post: ' . $post->titleRendered;
+        }
+    ] );
+});
 ?>
